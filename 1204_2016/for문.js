3b9d63e0c197006127d9faaaa660ed6f98b23d67
wooridle <wooridle@github.com>
2016-12-04 17:34:56 +0900

    hello

---
+++ 1204_2016/for문.js
@@ -0,0 +1,7 @@
+var fruits=[["apple","banana","berry","grape"],["melon","aaa","bbb","ccc","ddd"]
+
+for (var i = 0; i < fruits.length; i++) {
+    for (var i = 0; fruits.length; j++) {
+        console.log("fruits['+i+']['+j+']:" + fruitsi.length);
+    }
+}
